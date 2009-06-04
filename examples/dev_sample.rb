a2e03e5de62d1428ac39a2173d8086ae4a8a5459
func09 <mitsuru.haga@gmail.com>
2009-06-04 16:42:21 +0900

    実装をライブラリに移動した
    サンプルコードからlib/以下のクラスに移動
    実行ファイルの作成

--- examples/dev_sample.rb
+++ examples/dev_sample.rb
@@ -1,40 +1,15 @@
-require 'singleton'
-
-module RCron
-  
-  class Suite
-    include Singleton
-    attr_accessor :jobs
-    def initialize
-      @jobs = []
-    end
-    def job(schedule, &task)
-      jobs << ::RCron::Job.new(schedule, task)
-    end
-  end
-
-  class Job
-    attr_accessor :schedule, :task
-    def initialize(schedule,task)
-      self.schedule, self.task = schedule, task
-    end
-  end
-  
-end
-
-def rcron(&block)
-  RCron::Suite.instance.instance_eval &block
-  # for debug
-  p RCron::Suite.instance.jobs
-end
-
 # start DSL
 rcron do
+
+  # 毎分
   job '* * * * *' do
     p 'job test *'
   end
+
+  # ２分間隔
   job '*/2 * * * *' do
     p 'job test */2'
   end
+  
 end
 
--- lib/rcron.rb
+++ lib/rcron.rb
@@ -1,10 +1,49 @@
 require 'singleton'
 
+# Module RCron
 module RCron
   
+  module DSL
+    def rcron(&block)
+      RCron::Suite.instance.instance_eval &block
+    end
+  end
+  
+  # 
   class Suite
+    include Singleton
+    attr_accessor :jobs
+    def initialize
+      @jobs = []
+    end
+    def job(schedule, &task)
+      jobs << ::RCron::Job.new(schedule, task)
+    end
+    def run
+      @thread = Thread.new do
+        while true
+          begin
+            puts 'interval...'
+            @jobs.each do |job|
+              p job
+            end
+          rescue => e
+          end
+          sleep 1
+        end
+      end
+      @thread.join
+    end
   end
-
+  
+  # 
   class Job
+    attr_accessor :schedule, :task
+    def initialize(schedule,task)
+      self.schedule, self.task = schedule, task
+    end
   end
+  
 end
+
+include RCron::DSL
